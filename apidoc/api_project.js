0558a8c987c0c79ceb42a79ab7e0cbf57aff0657
charles <cfb3@uw.edu>
2021-04-12 14:54:46 -0700

    pushme: ready for lab 3?

---
+++ apidoc/api_project.js
@@ -0,0 +1,24 @@
+define({
+  "name": "TCSS 450 Lab Web Service",
+  "version": "0.1.0",
+  "description": "The TCSS 450 Lab Web Service API Documentation",
+  "title": "Lab API Doc",
+  "url": "https://cfb3-tcss450-labs-2021sp.herokuapp.com",
+  "order": [
+    "Hello",
+    "Params",
+    "Wait",
+    "DemoSql",
+    "Auth",
+    "Phish"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-12T21:53:34.091Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
